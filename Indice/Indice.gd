12cbba089105d9dbee0bd6c3fd4c4aee7e0fec1a
Edwin Saul <edwinsaulpm@gmail.com>
2022-07-18 23:32:06 -0500

    ejemplo e001 label

---
+++ Indice/Indice.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_E001_pressed():
+	get_tree().change_scene("res://Ejemplos/E001/e001.tscn")

4164f4d81a3f776138d24bdd12da8bbf3d0abd84
Edwin Saul <edwinsaulpm@gmail.com>
2022-07-20 09:16:03 -0500

    modificando boton salir

--- Indice/Indice.gd
+++ Indice/Indice.gd
@@ -1,24 +1,20 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+func _on_Boton_Salir_pressed():
+	get_tree().quit()
 
 func _on_E001_pressed():
 	get_tree().change_scene("res://Ejemplos/E001/e001.tscn")
 
 func _on_E002_pressed():
 	get_tree().change_scene("res://Ejemplos/E002/E002.tscn")
-	pass # Replace with function body.
+
+func _on_E003_pressed():
+	get_tree().change_scene("res://Ejemplos/E003/E003.tscn")
+
+func _on_E004_pressed():
+	get_tree().change_scene("res://Ejemplos/E004/E004.tscn")
+
